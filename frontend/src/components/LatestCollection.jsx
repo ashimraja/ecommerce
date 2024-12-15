87f226e05681014c34af9582a3e2239a44f4e50d
ashimraja <rajaasim652gmail.com>
2024-12-15 13:38:02 +0530

    Use Context

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+
+const LatestCollection = () => {
+
+    const {products} = useContext(ShopContext);
+
+    
+
+  return (
+    <div className="my-10">
+        <div className="text-center py-8 text-3xl">
+            <Title text1={'LATEST'} text2={'COLECTIONS'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora excepturi velit dicta est.</p>
+        </div>
+    </div>
+  )
+}
+
+export default LatestCollection
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </BrowserRouter>
 );
 
