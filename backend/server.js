12543d574da57f45a639b95700f36fc8ba7baaf0
ashimraja <rajaasim652gmail.com>
2024-12-17 22:58:50 +0530

    config

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDb from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 //App Config
 const app =express()
 const port = process.env.PORT || 4000
+connectDb()
+connectCloudinary();
 
 //middlewares
 app.use(express.json())
