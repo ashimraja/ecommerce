8d3367bc4b8401c11ae334b6bf2cdd97a5ecdd32
ashimraja <rajaasim652gmail.com>
2024-12-27 05:23:15 +0530

    cartcontroller

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,12 @@
+//add product to cart
+const addToCart= async(req,res)=>{
+
+}
+//update product to cart
+const updateCart= async(req,res)=>{
+
+}
+//get user product to cart
+const getUserCart= async(req,res)=>{
+
+}
